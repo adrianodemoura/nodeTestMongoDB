dc981f1cc7300be1a5997789a5ca83cb21047a27
Matheus Inocencio Lopes <inolopesm@gmail.com>
2022-03-22 14:12:57 -0300

    ajusta conexao com o mongoose

--- node_tests/login-controller-index.test.js
+++ node_tests/login-controller-index.test.js
@@ -1,16 +1,17 @@
 const request = require("supertest");
 const app = require("../src/app");
-const { dataBaseConnectionMongoDB } = require("../src/config/connection");
+const mongoose = require("mongoose");
 
 describe("Teste Inicial", () => {
   beforeAll(async () => {
-    //await mongoConnect()
-    //console.log('passei aqui no beforeAll')
+    await mongoose.connect("mongodb://localhost/nodejs", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
   });
 
   afterAll(async () => {
-    //await dataBaseConnectionMongoDB.close()
-    //console.log( 'passei aqui no afterAll' )
+    await mongoose.disconnect()
   });
 
   test("Testa se página inicial está ok", async () => {
