1a11ac87251259ae15d1f60d99bc414e08180d62
Adriano Carneiro de Moura <adrianodemoura@gmail.com>
2022-03-23 00:31:18 -0300

    mongo funcionando no teste

--- node_tests/unit/create-user.test.js
+++ node_tests/unit/create-user.test.js
@@ -1,20 +1,44 @@
 const faker = require("faker-br")
-const log = require("log")
 const User = require("../../src/app/Models/User")
+require("dotenv").config({
+  path: ".env_" + process.env.ENVIRONMENT.toLocaleLowerCase(),
+})
+const mongoose = require("mongoose")
 
 describe("Testes sobre o usuário", () => {
+  beforeAll(async () => {
+    await mongoose
+      .connect(`mongodb://${process.env.DB_CONNECT_PATH}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(
+        () => {},
+        (err) => {
+          console.log(err)
+        }
+      )
+  })
+
+  afterAll(async () => {
+    await mongoose.disconnect()
+  })
+
   it("O usuário deve ser criado", async () => {
-    const UserModel = new User({
-        name: faker.name.findName()
-      , email: faker.internet.email()
-      , password: faker.internet.password()
-      , municipio: faker.address.city()
-      , uf: faker.address.stateAbbr()
+    const UserModel = await new User({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      municipio: faker.address.city(),
+      uf: faker.address.stateAbbr(),
     })
 
-    log.info( UserModel )
-    
-    //expect(await UserModel.count({})).toBe(1)
+    await UserModel.save()
+      .then((doc) => {})
+      .catch((err) => {
+        console.log(err)
+      })
+
     expect(200).toBe(200)
   })
 })
